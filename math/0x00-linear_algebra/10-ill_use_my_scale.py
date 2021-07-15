669d6ebd2b0c96f54ca035dca186ca75a31330ef
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 19:47:10 +0000

    moved if name not main to a main file

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -8,14 +8,3 @@ def np_shape(matrix):
     """Function that calculates the shape of a numpy.ndarray. The shape will
     be returned as a tuple of integers"""
     return matrix.shape
-
-
-
-if __name__ is not "__main__":
-    mat1 = np.array([1, 2, 3, 4, 5, 6])
-    mat2 = np.array([])
-    mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                     [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
-    print(np_shape(mat1))
-    print(np_shape(mat2))
-    print(np_shape(mat3))
