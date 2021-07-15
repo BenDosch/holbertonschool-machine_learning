3e96c53ae8f9e111688f22f30ed7f75f58112706
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 19:52:18 +0000

    pycodestylefix

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module that contains the function np_shape"""
 
+
 def np_shape(matrix):
     """Function that calculates the shape of a numpy.ndarray. The shape will
     be returned as a tuple of integers"""
