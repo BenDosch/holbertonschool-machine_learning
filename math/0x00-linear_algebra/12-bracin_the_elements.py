93c79834a48a102119c8255dc4c2f811c1bc626c
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 20:22:13 +0000

    task 12 ready to check

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 """ Module that contains the function np_elementwise"""
 
-import numpy as np
-
 
 def np_elementwise(mat1, mat2):
-    """F"""
-
-
-if __name__ is not "__main__":
-    mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-    mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-    
-    print(mat1)
-    print(mat2)
-    add, sub, mul, div = np_elementwise(mat1, mat2)
-    print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
-    add, sub, mul, div = np_elementwise(mat1, 2)
-    print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
+    """Function that performs element-wise addition, subtraction,
+    multiplication, and division. Assumes that mat1 and mat2 can be
+    interpreted as numpy.ndarrays. Assumes that mat1 and mat2 are never empty.
+    Returns a tuple containing the element-wise sum, difference, product, and
+    quotient, respectively."""
+    addsum = mat1 + mat2
+    dif = mat1 - mat2
+    prod = mat1 * mat2
+    quot = mat1 / mat2
+    return (addsum, dif, prod, quot)
