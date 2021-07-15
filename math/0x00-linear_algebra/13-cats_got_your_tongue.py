b7bb8f8a77b0731a716adfa7a2c9c6cf4c91ac3e
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 20:43:19 +0000

    task 13 ready for checker

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -5,12 +5,8 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """F"""
-
-if __name__ is not "__main__":
-    mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-    mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-    mat3 = np.array([[7], [8]])
-    print(np_cat(mat1, mat2))
-    print(np_cat(mat1, mat2, axis=1))
-    print(np_cat(mat1, mat3, axis=1))
+    """Function that concatenates two matrices along a specific axis. Assumes
+    that mat1 and mat2 can be interpreted as numpy.ndarrays. Assumes that mat1
+    and mat2 are never empty. Returns a new numpy.ndarray"""
+    new = np.concatenate((mat1, mat2), axis)
+    return new
