34d94c3e528d086cec2f1f690118246f94fc6d8e
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 19:49:50 +0000

    trying it without the import

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 """ Module that contains the function np_shape"""
 
-import numpy as np
-
-
 def np_shape(matrix):
     """Function that calculates the shape of a numpy.ndarray. The shape will
     be returned as a tuple of integers"""
