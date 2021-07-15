bb60b317663b544e970bd17ee206cb7f8e6a55dc
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 20:03:55 +0000

    trying without the import

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """ Module that contains the function np_transpose"""
 
-import numpy as np
-
 
 def np_transpose(matrix):
     """Function that transposes matrix. Assumes that matrix can be interpreted
