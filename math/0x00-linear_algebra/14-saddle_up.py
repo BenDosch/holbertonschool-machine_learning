38c5ea1138bfa1430f605a76eb924c008c02ddd4
BenDoschGit <Benjamindosch@gmail.com>
2021-07-15 20:46:32 +0000

    task 14 ready for checker

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -5,12 +5,6 @@ import numpy as np
 
 
 def np_matmul(mat1, mat2):
-    """F"""
-
-
-if __name__ is not "__main__":
-    mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-    mat2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    mat3 = np.array([[7], [8], [9]])
-    print(np_matmul(mat1, mat2))
-    print(np_matmul(mat1, mat3))
+    """Function that performs matrix multiplication. Assumes that mat1 and mat2
+    are numpy.ndarrays. Assumes that mat1 and mat2 are never empty"""
+    return mat1 @ mat2
