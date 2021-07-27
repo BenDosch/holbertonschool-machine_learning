4795b2a4511e83d72dd244fc22f1b58eaa3695d4
Ben Dosch <benjamindosch@gmail.com>
2021-07-27 14:12:39 -0500

    spelling correction

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -24,7 +24,7 @@ class Poisson():
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError("data must contain multipule values")
+                raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
         else:
             self.lambtha = float(lambtha)
