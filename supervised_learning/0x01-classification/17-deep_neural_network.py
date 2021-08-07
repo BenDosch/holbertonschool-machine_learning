e949fce0b1571826a8ce983150b4e6932c50737b
Ben Dosch <benjamindosch@gmail.com>
2021-08-07 14:09:37 -0500

    changes to some conditionals

--- supervised_learning/0x01-classification/17-deep_neural_network.py
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -21,7 +21,7 @@ class DeepNeuralNetwork():
             raise TypeError("nx must be an integer")
         elif nx < 1:
             raise ValueError("nx must be a positive integer")
-        elif not isinstance(layers, list) or len(layers) == 0:
+        elif not isinstance(layers, list) or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
@@ -29,7 +29,7 @@ class DeepNeuralNetwork():
         weights = {}
         prev = nx
         for i, l in enumerate(layers, 1):
-            if not isinstance(l, int) and l < 1:
+            if not isinstance(l, int) and l < 0:
                 raise TypeError("layers must be a list of positive integers")
             weights["b{}".format(i)] = np.zeros((l, 1))
             weights["W{}".format(i)] = (np.random.randn(l, prev) *
