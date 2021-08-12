18c91f6c7718af5b68ec76f95245956137e95761
Ben Dosch <benjamindosch@gmail.com>
2021-08-12 14:38:47 -0500

    Getting right output locally

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -2,8 +2,8 @@
 """Module containing the function create_ layer.
 """
 
-import numpy as np
 import tensorflow as tf
+
 
 def create_layer(prev, n, activation):
     """Function that creates a layer of a neural network using tesnorflow.
@@ -12,8 +12,11 @@ def create_layer(prev, n, activation):
         prev ([type]): The tensor output of the previous layer.
         n ([type]): The number of nodes in the layer to create.
         activation ([type]): The activation function that the layer should use.
-    
+
     Returns:
         The tensor output of the layer
     """
-    # Code
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, activation=activation, name="layer",
+                            kernel_initializer=init)
+    return layer(prev)
