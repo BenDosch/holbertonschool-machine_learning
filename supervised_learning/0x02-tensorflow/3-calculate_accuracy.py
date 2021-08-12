695eb4bf5c7ea1e9206c5945bad0ab6e8cd472d9
Ben Dosch <benjamindosch@gmail.com>
2021-08-12 16:13:18 -0500

    getting correct local output

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -2,8 +2,8 @@
 """Module containing the function calculate_accuracy.
 """
 
-import numpy as np
 import tensorflow as tf
+
 
 def calculate_accuracy(y, y_pred):
     """Function that calculates the accuracy of a prediction.
@@ -15,4 +15,6 @@ def calculate_accuracy(y, y_pred):
     Returns:
         Tensor containing the decimal accuracy of the prediction.
     """
-    # Code
+    correct_prediction = tf.equal(tf.argmax(y), tf.argmax(y_pred))
+    mean = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return mean
