61810fd653affde4989269d273a423afe3f582e4
Ben Dosch <benjamindosch@gmail.com>
2021-08-17 15:56:56 -0500

    style checked and ready for checker

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -2,6 +2,7 @@
 """Module containing the function update_variables_RMSProp.
 """
 
+
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Function that
 
@@ -13,8 +14,13 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
             updated.
         grad (numpy.ndarray): N-dimensional array containing the gradient of
             var.
-        s ([type]): The previous second moment of var.
-    
+        s float): The previous second moment of var.
+
     Returns:
         The updated variables and the new moment, respectively.
     """
+    #  e.g. SdW = (beta)SdW + (1 - Beta)(dW ** 2)
+    moment = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    #  e.g. W = W - (alpha)(dW / (SdW ** (0.5)))
+    updated = var - (alpha * (grad / ((moment ** (0.5) + epsilon))))
+    return updated, moment
