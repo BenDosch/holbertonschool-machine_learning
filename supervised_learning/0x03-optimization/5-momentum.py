5f76beadbd6906137d6ed3eeae39f31ee501c2ae
Ben Dosch <benjamindosch@gmail.com>
2021-08-17 15:34:31 -0500

    stylechecked and ready for checker

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -2,8 +2,6 @@
 """Module containing the function update_variables_momentum.
 """
 
-import numpy as np
-
 
 def update_variables_momentum(alpha, beta1, var, grad, V):
     """Function that updates a variable using the gradient descent with
@@ -13,8 +11,16 @@ def update_variables_momentum(alpha, beta1, var, grad, V):
         alpha (float): The learning rate.
         beta1 (float): The momentum weight.
         var (numpy.ndarray): N-dimensional array that conatains the variable
-            to be updated.
+            to be updated. e.g. (dW or db)
         grad (numpy.ndarray): N-dimensional array that conatains the gradient
             of var.
-        V ([type]): The previous first moment of var.
+        V (float): The previous first moment of var.
+
+    Returns:
+        The updated variable and the new moment, respectively.
     """
+    moment = (beta1 * V) + ((1 - beta1) * grad)
+    #  e.g. V_dW = (beta)V_dW + (1-beta)dW
+    updated = var - (alpha * moment)
+    #  e.g. W = W - (alpha)V_dW
+    return updated, moment
