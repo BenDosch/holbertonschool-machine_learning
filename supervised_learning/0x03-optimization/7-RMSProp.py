f1dc2d92a80433aad6c2abbf323115a99c7c06ec
Ben Dosch <benjamindosch@gmail.com>
2021-08-17 16:02:32 -0500

    documentation update

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -19,7 +19,7 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     Returns:
         The updated variables and the new moment, respectively.
     """
-    #  e.g. SdW = (beta)SdW + (1 - Beta)(dW ** 2)
+    #  e.g. SdW = (beta2)SdW + (1 - beta2)(dW ** 2)
     moment = (beta2 * s) + ((1 - beta2) * (grad ** 2))
     #  e.g. W = W - (alpha)(dW / (SdW ** (0.5)))
     updated = var - (alpha * (grad / ((moment ** (0.5) + epsilon))))
