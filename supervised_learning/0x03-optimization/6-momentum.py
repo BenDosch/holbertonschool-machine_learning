7c9b276e2b4c6f5981d61bc3d9ebfd750b2d5f65
Ben Dosch <benjamindosch@gmail.com>
2021-08-17 15:44:45 -0500

    style checked and ready for checker

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -4,9 +4,11 @@
 
 import tensorflow as tf
 
+
 def create_momentum_op(loss, alpha, beta1):
     """Function that that creates the training operation for a neural network
-    in tensorflow using the gradient descent with momentum optimization algorithm.
+    in tensorflow using the gradient descent with momentum optimization
+    algorithm.
 
     Args:
         loss (float): The loss of the network.
@@ -16,3 +18,4 @@ def create_momentum_op(loss, alpha, beta1):
     Returns:
         The momentum optimization operation.
     """
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
