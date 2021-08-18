a82c0c77b0ba4705766a0dd0204a39a75db748e0
Ben Dosch <benjamindosch@gmail.com>
2021-08-18 16:31:02 -0500

    some clean up and removed the init of the session.

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -34,39 +34,39 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     shuffle_data = __import__('2-shuffle_data').shuffle_data
     sess = tf.Session()
     saver = tf.train.import_meta_graph(load_path + '.meta')
+    store = tf.train.Saver()
     saver.restore(sess, load_path)
     x = tf.get_collection('x')[0]
     y = tf.get_collection('y')[0]
     accuracy = tf.get_collection('accuracy')[0]
     loss = tf.get_collection('loss')[0]
     train_op = tf.get_collection('train_op')[0]
-    init = tf.global_variables_initializer()
-    store = tf.train.Saver()
-    
-    sess.run(init)
 
     for epoch in range(epochs + 1):
         train = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
         valid = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+        
         print("After {} epochs:".format(epoch))
         print("\tTraining Cost: {}".format(train[0]))
         print("\tTraining Accuracy: {}".format(train[1]))
         print("\tValidation Cost: {}".format(valid[0]))
         print("\tValidation Accuracy: {}".format(valid[1]))
+
         if epoch < epochs:
             X_train_s, Y_train_s = shuffle_data(X_train, Y_train)
-            step = 0
             m = X_train.shape[0]
 
             if m % batch_size == 0:
-                total_batches = m / batch_size
+                total_batches = m // batch_size
             else:
-                total_batches = int(m / batch_size) + 1
+                total_batches = (m // batch_size) + 1
 
             for batch in range(total_batches):
                 batch_start = batch * batch_size
                 batch_end = batch_start + batch_size
-                if batch_end >= m:
+                step = batch + 1
+
+                if batch_end > m:
                     X_t = X_train[batch_start:, :]
                     Y_t = Y_train_s[batch_start:, :]
                 else:
@@ -74,7 +74,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
                     Y_t = Y_train_s[batch_start:batch_end, :]
                 results = sess.run([accuracy, loss, train_op],
                                     feed_dict={x: X_t, y: Y_t})
-                step += 1
+                
                 if step % 100 == 0:
                     print("\tStep {}:".format(step))
                     print("\t\tCost: {}".format(results[1]))
