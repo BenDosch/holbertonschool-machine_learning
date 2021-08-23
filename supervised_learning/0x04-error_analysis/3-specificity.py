cf80eb913c982d009e767ca174085c90baf7da76
Ben Dosch <benjamindosch@gmail.com>
2021-08-23 16:21:41 -0500

    ready for checker

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -13,8 +13,13 @@ def specificity(confusion):
         confusion (numpy.ndarray): A confusion N-dimensional array  of shape
             (classes, classes) where row indices represent the correct labels
             and column indices represent the predicted labels.
-    
+
     Returns:
         A N-dimensional array of shape (classes,) containing the specificity
         of each class.
     """
+    true_pos = np.diag(confusion)
+    false_pos = np.sum(confusion, axis=0) - true_pos
+    false_neg = np.sum(confusion, axis=1) - true_pos
+    true_neg = np.sum(confusion) - (true_pos + false_pos + false_neg)
+    return (true_neg / (true_neg + false_pos))
