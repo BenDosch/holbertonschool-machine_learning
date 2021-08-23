93a41cc561f41d6ec1344c973c6b6e47075d0295
Ben Dosch <benjamindosch@gmail.com>
2021-08-23 14:29:16 -0500

    ready for checker

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -8,13 +8,16 @@ import numpy as np
 def sensitivity(confusion):
     """Function that calculates the sensitivity for each class in a confusion
     matrix.
-    
+
     Args:
         confusion (numpy.ndarray): A confusion N-dimensional array  of shape
             (classes, classes) where row indices represent the correct labels
             and column indices represent the predicted labels.
-    
+
     Returns:
         A N-dimensional array of shape (classes,) containing the sensitivity
         of each class.
     """
+    c_totals = np.sum(confusion, axis=1)
+    t_positives = np.sum(confusion * np.identity(confusion.shape[0]), axis=0)
+    return t_positives / c_totals.T
