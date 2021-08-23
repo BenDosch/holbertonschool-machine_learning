07bec3363d5274b21e42b57c87e3b7865a6d89af
Ben Dosch <benjamindosch@gmail.com>
2021-08-23 14:36:57 -0500

    ready for checker

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -8,13 +8,16 @@ import numpy as np
 def precision(confusion):
     """Function that calculates the precision for each class in a confusion
     matrix.
-    
+
     Args:
         confusion (numpy.ndarray): A confusion N-dimensional array  of shape
             (classes, classes) where row indices represent the correct labels
             and column indices represent the predicted labels.
-    
+
     Returns:
         A N-dimensional array of shape (classes,) containing the precision
         of each class.
     """
+    total_positive = np.sum(confusion, axis=0)
+    t_positives = np.sum(confusion * np.identity(confusion.shape[0]), axis=0)
+    return t_positives / total_positive.T
