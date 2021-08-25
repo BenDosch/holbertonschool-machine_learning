5f000d864ea5269af4c3b102ea3be577965d9f66
Ben Dosch <benjamindosch@gmail.com>
2021-08-25 15:35:55 -0500

    ready for checker

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -5,11 +5,11 @@
 import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Function
+    """Function that creates a tensorflow layer that includes L2
+    regularization.
 
     Args:
-        prev (tensor): A hat creates a tensorflow layer that includes L2
-            regularization.
+        prev (tensor): A tensor containing the output of the previous layer.
         n (int): The number of nodes the new layer should contain.
         activation (float): The activation function that should be used on the
             layer.
@@ -18,4 +18,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Returns:
         The output of the new layer
     """
-    # Code
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    reg = tf.contrib.layers.l2_regularizer(scale=lambtha)
+    layer = tf.layers.Dense(units=n, activation=activation, name="layer",
+                            kernel_initializer=init, kernel_regularizer=reg)
+    return layer(prev) 
