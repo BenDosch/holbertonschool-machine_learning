c09c879e12ff771a010da3a3848cdcbf7f216d05
Ben Dosch <benjamindosch@gmail.com>
2021-08-25 13:42:38 -0500

    ready for checker

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -22,4 +22,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         lambtha (float): The L2 regularization parameter.
         L (int): The number of layers of the network
     """
-    # Code
+    m = Y.shape[1]
+    for layer in range(L, 0, -1):
+        Ai = cache["A{}".format(layer)]
+        Ai_next = cache["A{}".format(layer - 1)]
+        if layer == L:
+            dZi = (Ai - Y)
+        else:
+            dZi = dAi_next * 1 - (Ai ** 2)  # tanh_prime
+        Wi = weights["W{}".format(layer)]
+        dWi = (np.matmul(dZi, Ai_next.T) / m) + ((lambtha / m) * Wi)
+        dbi = np.sum(dZi, axis=1, keepdims=True) / m
+        dAi_next = np.matmul(Wi.T, dZi)
+        weights["W{}".format(layer)] -= (alpha * dWi)
+        weights["b{}".format(layer)] -= (alpha * dbi)
