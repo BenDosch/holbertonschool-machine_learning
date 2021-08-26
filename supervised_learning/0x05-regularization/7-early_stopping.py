533774e16e0c0079d72bf339307e494472cbf951
Ben Dosch <benjamindosch@gmail.com>
2021-08-26 14:09:38 -0500

    reayd for checker

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -20,4 +20,11 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
         A boolean of whether the network should be stopped early, followed by
         the updated count.
     """
-    # Code
+    if (cost - opt_cost) > threshold:
+        count = 0
+    else:
+        count += 1
+    if count >= patience:
+        return True, count
+    else:
+        return False, count
