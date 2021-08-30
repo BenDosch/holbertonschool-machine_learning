4bf7eb6cc04e85b0474b366228ef813821a54af8
Ben Dosch <benjamindosch@gmail.com>
2021-08-30 18:18:36 -0500

    ready for checker

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -12,7 +12,7 @@ def train_model(network, data, labels, batch_size, epochs,
     also trains the model using early stopping.
 
     Args:
-        network ([type]): The model to train.
+        network (keras.Model): The model to train.
         data (numpy.ndarray): A numpy.ndarray of shape (m, nx) containing the
             input data.
         labels (numpy.ndarray): A one-hot numpy.ndarray of shape (m, classes)
@@ -21,8 +21,8 @@ def train_model(network, data, labels, batch_size, epochs,
             gradient descent.
         epochs (int): The number of passes through data for mini-batch
             gradient descent.
-        validation_data([type]): The data to validate the model with. Defaults
-            to None.
+        validation_data(tuple, optional): The data to validate the model with.
+            Defaults to None.
         early_stopping([bool, optional]): A boolean that indicates whether
             early stopping should be used.
         patience([int]): The patience used for early stopping.
@@ -34,4 +34,21 @@ def train_model(network, data, labels, batch_size, epochs,
     Returns:
         The History object generated after training the model.
     """
-    # Code         
+    if early_stopping is not None and validation_data is not None:
+        e_s = K.callbacks.EarlyStopping(
+                monitor='validation_loss',
+                patience=patience
+            )
+    else:
+        e_s = None
+
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        verbose=verbose,
+        callbacks=[e_s],
+        shuffle=shuffle
+    )
