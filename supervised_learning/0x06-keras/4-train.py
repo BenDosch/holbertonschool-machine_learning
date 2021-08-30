d1e9ae24d2ffc8efdcc5a95c6bd562ea5fc468c7
Ben Dosch <benjamindosch@gmail.com>
2021-08-30 18:01:34 -0500

    ready for checker

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -4,12 +4,13 @@
 
 import tensorflow.keras as K
 
+
 def train_model(network, data, labels, batch_size, epochs, verbose=True,
                 shuffle=False):
     """Function that trains a model using mini-batch gradient descent.
 
     Args:
-        network ([type]): The model to train.
+        network (keras.Model): The model to train.
         data (numpy.ndarray): A numpy.ndarray of shape (m, nx) containing the
             input data.
         labels (numpy.ndarray): A one-hot numpy.ndarray of shape (m, classes)
@@ -26,4 +27,11 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True,
     Returns:
         The History object generated after training the model.
     """
-    # Code         
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
