0025eb5bd7644e8f1ded62ce9d6ddfca50da1904
Ben Dosch <benjamindosch@gmail.com>
2021-08-30 16:49:49 -0500

    ready for checker

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -10,9 +10,14 @@ def optimize_model(network, alpha, beta1, beta2):
     categorical crossentropy loss and accuracy metrics.
 
     Args:
-        network ([type]): The model to optimize.
+        network (keras.Model): The model to optimize.
         alpha (float): The learning rate.
         beta1 (float): The first Adam optimization parameter.
         beta2 (float): The second Adam optimization parameter.
     """
-    # Code
+    opt = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(
+        optimizer=opt,
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
