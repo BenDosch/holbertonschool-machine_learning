b324a10e3b7dfca1ac0fd8238e6f8ba2ecff6b5e
Ben Dosch <benjamindosch@gmail.com>
2021-08-30 16:09:53 -0500

    ready for checker

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -12,12 +12,29 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         nx (int): The number of input features to the network.
         layers (list[int]): A  list containing the number of nodes in each
             layer of the network.
-        activations (list[str]): A list containing the activation functions used
-            for each layer of the network.
+        activations (list[str]): A list containing the activation functions
+            used for each layer of the network.
         lambtha (float): The L2 regularization parameter.
-        keep_prob (float): The probability that a node will be kept for dropout.
+        keep_prob (float): The probability that a node will be kept for
+            dropout.
 
     Returns:
         The keras model.
     """
-    # Code
+    model = K.Sequential()
+    reg = K.regularizers.l2(lambtha)
+    for l in range(len(layers)):
+        if l is 0:
+            model.add(K.layers.Dense(units=layers[l],
+                                     activation=activations[l],
+                                     kernel_regularizer=reg,
+                                     input_shape=(nx,)))
+        else:
+            model.add(K.layers.Dense(
+                units=layers[l],
+                activation=activations[l],
+                kernel_regularizer=reg
+            ))
+            model.add(K.layers.Dropout(1 - keep_prob))
+
+    return model
