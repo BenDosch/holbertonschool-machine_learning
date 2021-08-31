57eaef5b73ceba15c2247f8b2578f814416e53d1
Ben Dosch <benjamindosch@gmail.com>
2021-08-31 14:40:33 -0500

    removed the unessicary argument for to_json

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -17,8 +17,8 @@ def save_config(network, filename):
     Returns:
         None
     """
-    json_string = network.to_json(filename)
-    with open(filename, 'w+') as file:
+    json_string = network.to_json()
+    with open(filename, 'w') as file:
         file.write(json_string)
 
     return None
