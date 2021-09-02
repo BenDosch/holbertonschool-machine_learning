6e5e13f470b9c139b51f818c7565edc1e3e46f45
Ben Dosch <benjamindosch@gmail.com>
2021-09-02 13:43:01 -0500

    added a missing space

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -30,7 +30,7 @@ def convolve_grayscale_same(images, kernel):
         pad_w = (kw - 1) // 2
     else:
         pad_w = kw // 2
-    images = np.pad(images, ((0,0), (pad_h, pad_h), (pad_w, pad_w)),
+    images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
                     "constant", constant_values=0)
     convol = np.zeros((m, h, w))
     for x in range(convol.shape[1]):
