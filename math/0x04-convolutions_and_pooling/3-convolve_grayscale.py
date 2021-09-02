68a3e05fb904602b9c95df60ae45e13eef41ee8f
Ben Dosch <benjamindosch@gmail.com>
2021-09-02 14:01:20 -0500

    changed loop paramaters

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -46,8 +46,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     conv_w = ((w + (2 * pad_w) - kw) // sw) + 1
     convol = np.zeros((m, conv_h, conv_w))
 
-    for x in range(0, convol.shape[1], sh):
-        for y in range(0, convol.shape[2], sh):
+    for x in range(0, (h + (2 * pad_h) - kh), sh):
+        for y in range(0, (w + (2 * pad_w) - kw), sh):
             output = np.sum(images[:, x: x + kh, y: y + kw] * kernel,
                             axis=(1, 2))
             convol[:, x, y] = output
