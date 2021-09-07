f386b46e16e92885b16d7bbc4369f3f2f3d37cb7
Ben Dosch <benjamindosch@gmail.com>
2021-09-07 00:28:46 -0500

    clean up and documentaion improvements

--- supervised_learning/0x07-cnn/1-pool_forward.py
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -28,4 +28,22 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Returns:
         The output of the pooling layer.
     """
-    # Code
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    output_h = ((h_prev - kh) // sh) + 1
+    output_w = ((w_prev - kw) // sw) + 1
+    pooled = np.zeros((m, output_h, output_w, c_prev))
+    i = 0
+    for x in range(0, (h_prev - kh + 1), sh):
+        j = 0
+        for y in range(0, w_prev - kw + 1, sw):
+            if mode == 'max':
+                pooled[:, i, j, :] = np.max(A_prev[:, x:x + kh, y:y + kw, :],
+                                            axis=(1, 2))
+            elif mode == 'avg':
+                pooled[:, i, j, :] = np.average(A_prev[:, x:x + kh, y:y + kw, :],
+                                    axis=(1, 2))
+            j += 1
+        i += 1
+    return pooled
