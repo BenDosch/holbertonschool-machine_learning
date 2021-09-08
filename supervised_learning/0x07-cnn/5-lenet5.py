838af56591f88160155ec101418372353536b4b6
Ben Dosch <benjamindosch@gmail.com>
2021-09-07 21:09:31 -0500

    Ready for checker

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -14,8 +14,39 @@ def lenet5(X):
             the input images for the network where m is the number of images.
 
     Returns:
-        Returns:
-            A tensorflow.keras.Model compiled to use Adam optimization
-            (with default hyperparameters) and accuracy metrics.
+        A tensorflow.keras.Model compiled to use Adam optimization
+        (with default hyperparameters) and accuracy metrics.
     """
-    # Code
+
+    init = K.initializers.he_normal(seed=None)
+    C1 = K.layers.Conv2D(
+        filters=6, kernel_size=(5, 5), strides=(1, 1), padding="same",
+        activation="relu", kernel_initializer=init
+    )(X)
+    S2 = K.layers.MaxPool2D(
+        pool_size=(2, 2), strides=(2, 2), padding="valid"
+    )(C1)
+    C3 = K.layers.Conv2D(
+        filters=16, kernel_size=(5, 5), strides=(1, 1), padding="valid",
+        activation="relu", kernel_initializer=init
+    )(S2)
+    S4 = K.layers.MaxPool2D(
+        pool_size=(2, 2), strides=(2, 2), padding="valid"
+    )(C3)
+    S4 = K.layers.Flatten()(S4)
+    C5 = K.layers.Dense(
+        units=120, activation="relu", kernel_initializer=init
+    )(S4)
+    F6 = K.layers.Dense(
+        units=84, activation="relu", kernel_initializer=init
+    )(C5)
+    OUTPUT = K.layers.Dense(
+        units=10, activation="softmax", kernel_initializer=init
+    )(F6)
+
+    # Complie
+    opt = K.optimizers.Adam()
+    model = K.Model(inputs=X, outputs=OUTPUT)
+    model.compile(opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
