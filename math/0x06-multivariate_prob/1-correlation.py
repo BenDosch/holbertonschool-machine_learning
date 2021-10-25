ff7da2711cdfc7e5556a0991b14649ee00e2bc0a
Ben Dosch <benjamindosch@gmail.com>
2021-10-25 18:28:28 -0500

    feels like they forgot to forbid np.corrcef

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -4,6 +4,7 @@ correlation matrix."""
 
 import numpy as np
 
+
 def correlation(C):
     """Function that calculates a correlation matrix.
 
@@ -18,11 +19,13 @@ def correlation(C):
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
 
-    if (not len(c.shape) == 2) or (not C.shape[0] == C.shape[1]):
+    if (not len(C.shape) == 2) or (not C.shape[0] == C.shape[1]):
         raise ValueError("C must be a 2D square matrix")
 
-    correlation_matrix = 
+    correlation_matrix = np.corrcoef(C, rowvar=False)
+    
     return correlation_matrix
+
 
 if __name__ == "__main__":
     C = np.array([[36, -30, 15], [-30, 100, -20], [15, -20, 25]])
