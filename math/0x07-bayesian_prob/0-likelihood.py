c25ad7d47a5e9466acf34a65ddddd33bc03b1d08
Ben Dosch <benjamindosch@gmail.com>
2021-10-31 13:51:11 -0500

    Type-o fixed

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -24,10 +24,12 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+            )
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not (isinstance(P, np.ndarray) and len(P.shpae) == 1):
+    if not (isinstance(P, np.ndarray) and len(P.shape) == 1):
         raise TypeError("P must be a 1D numpy.ndarray")
     if any([True if (x > 1 or x < 0) else False for x in P]):
         raise ValueError("All values in P must be in the range [0, 1]")
