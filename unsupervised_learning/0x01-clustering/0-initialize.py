3e39a17c9551b66cedc4a3f7baca3b88d9f56b03
Ben Dosch <benjamindosch@gmail.com>
2021-11-02 10:51:39 -0500

    checker is picking up commented import, trying new tactic

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -31,7 +31,7 @@ def initialize(X, k):
 
 
 if __name__ == "__main__":
-    # import matplotlib.pyplot as plt
+    """import matplotlib.pyplot as plt"""
 
     np.random.seed(0)
     a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
