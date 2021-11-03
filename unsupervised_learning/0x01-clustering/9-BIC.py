35913216d63e89276612f8b9963fc5846c71b9ff
Ben Dosch <benjamindosch@gmail.com>
2021-11-03 16:24:25 -0500

    ready for checker

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -3,18 +3,25 @@
 for a GMM using the Bayesian Information Criterion."""
 
 import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """Function that finds the best number of clusters for a GMM using the Bayesian Information Criterion.
+    """Function that finds the best number of clusters for a GMM using the
+    Bayesian Information Criterion.
 
     Args:
-        X ([type]): [description]
-        kmin (int, optional): [description]. Defaults to 1.
-        kmax ([type], optional): [description]. Defaults to None.
-        iterations (int, optional): [description]. Defaults to 1000.
-        tol ([type], optional): [description]. Defaults to 1e-5.
-        verbose (bool, optional): [description]. Defaults to False.
+        X (numpy.ndarray): A tensor of shape (n, d) containing the data set.
+        kmin (int, optional): A positive integer containing the minimum number
+            of clusters to check for (inclusive). Defaults to 1.
+        kmax (int, optional): A positive integer containing the maximum number
+            of clusters to check for (inclusive). Defaults to None.
+        iterations (int, optional): A positive integer containing the maximum
+            number of iterations for the EM algorithm. Defaults to 1000.
+        tol (float, optional): A non-negative float containing the tolerance
+            for the EM algorithm. Defaults to 1e-5.
+        verbose (bool, optional): A boolean that determines if the EM algorithm
+            should print information to the standard output. Defaults to False.
 
     Returns:
         best_k is the best value for k based on its BIC.
@@ -37,22 +44,51 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
                 l (float): The log likelihood of the model.
         None, None, None, None on failure.
 """
-    best_k = None
-    best_result = None
-    l = None
-    b = None
-    return best_k, best_result, l, b
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(kmin, int) or kmin <= 0 or kmin > X.shape[0] or
+            not isinstance(kmax, int) or kmax <= 0 or kmax < kmin or
+            kmax > X.shape[0] or not isinstance(iterations, int) or
+            iterations <= 0 or not isinstance(tol, float) or tol < 0 or
+            not isinstance(verbose, bool)):
+        return None, None, None, None
+
+    n, d = X.shape
+    l_arr = np.empty((kmax - kmin + 1))
+    b = np.empty((kmax - kmin + 1))
+    results = [()] * (kmax - kmin + 1)
+
+    for k in range(kmin, kmax + 1):
+        index = k - kmin
+        pi, m, S, g, log = expectation_maximization(
+            X, k, iterations, tol, verbose
+        )
+
+        l_arr[index] = log
+        results[index] = (pi, m, S)
+
+        p = (k * d) + (k * (d * ((d + 1)) / 2)) + (k - 1)
+        BIC = (p * np.log(n)) - (2 * log)
+        b[index] = BIC
+
+    best_result = results[np.argmin(b)]
+    best_k = np.argmin(b) + kmin
+
+    return best_k, best_result, l_arr, b
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
+    """impo matplotlib.pyplot as plt
 
     np.random.seed(11)
     a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
     b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
     c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
-    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    d = np.random.multivariate_normal(
+        [20, 70], [[35, 10], [10, 35]], size=1000
+    )
     X = np.concatenate((a, b, c, d), axis=0)
     np.random.shuffle(X)
     best_k, best_result, l, b = BIC(X, kmin=1, kmax=10)
@@ -70,4 +106,4 @@ if __name__ == "__main__":
     plt.xlabel('Clusters')
     plt.ylabel('BIC')
     plt.tight_layout()
-    plt.show()
+    plt.show()"""
