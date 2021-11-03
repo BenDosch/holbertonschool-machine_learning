aa15ce3de3728eb9ba3a994d6dc0d056048ecee4
Ben Dosch <benjamindosch@gmail.com>
2021-11-03 13:51:24 -0500

    ready for checker

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -13,7 +13,7 @@ def maximization(X, g):
         X (numpy.ndarray): A tensor of shape (n, d) containing the data set.
         g (numpy.ndarray): A tensor of shape (k, n) containing the posterior
             probabilities for each data point in each cluster.
-    
+
     Returns:
         pi (numpy.ndarray) A tensor of shape (k,) containing the updated priors
             for each cluster.
@@ -23,22 +23,41 @@ def maximization(X, g):
             covariance matrices for each cluster.
         None, None, None on failure.
     """
-    pi = None
-    m = None
-    S = None
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(g, np.ndarray) or g.ndim != 2 or
+            X.shape[0] != g.shape[1]):
+        return None, None, None
+
+    n, d = X.shape
+    k = g.shape[0]
+    pi = np.empty((k))
+    m = np.empty((k, d))
+    S = np.empty((k, d, d))
+
+    for i in range(k):
+        # g = γ, expected value, posterior probablity, W
+        sum_gi = np.sum(g[i], axis=0)
+        # pi = 1/n sum(g)
+        pi[i] = sum_gi / n
+        # mu = sum(g * X) / sum(g)
+        m[i] = np.sum(g[i, None, ...] @ X, axis=0) / sum_gi
+        # sigma = sum(g) * (X - mu)(X - mu).T / sum(g), summed in broadcast?
+        S[i] = ((g[i, None, ...] * (X - m[i]).T) @ (X - m[i])) / sum_gi
+
     return pi, m, S
 
 
 if __name__ == "__main__":
-    initialize = __import__('4-initialize').initialize
-    expectation = __import__('6-expectation').expectation
-
+    """initialize = __impo  __('4-initialize').initialize
+    expectation = __impo  __('6-expectation').expectation
 
     np.random.seed(11)
     a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
     b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
     c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
-    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    d = np.random.multivariate_normal(
+        [20, 70], [[35, 10], [10, 35]], size=1000
+        )
     X = np.concatenate((a, b, c, d), axis=0)
     np.random.shuffle(X)
     pi, m, S = initialize(X, 4)
@@ -46,4 +65,4 @@ if __name__ == "__main__":
     pi, m, S = maximization(X, g)
     print(pi)
     print(m)
-    print(S)
+    print(S)"""
