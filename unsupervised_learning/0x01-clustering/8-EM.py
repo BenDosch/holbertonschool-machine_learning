c75d55d24186dfa886114afcb1671bb57a94bef6
Ben Dosch <benjamindosch@gmail.com>
2021-11-03 14:47:52 -0500

    ready for checker

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -35,23 +35,46 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         l (float): The log likelihood of the model.
         None, None, None, None, None on failure.
     """
-    pi = None
-    m = None
-    S = None
-    g = None
-    l = None
-    return pi, m, S, g, l
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(k, int) or k <= 0 or k > X.shape[0] or
+            not isinstance(iterations, int) or iterations <= 0 or
+            not isinstance(tol, float) or tol < 0 or
+            not isinstance(verbose, bool)):
+        return None, None, None, None, None
+
+    n, d = X.shape
+    pi, m, S = initialize(X, k)
+    l_prev = 0
+
+    for i in range(iterations):
+        if i != 0:
+            l_prev = log
+        g, log = expectation(X, pi, m, S)
+        pi, m, S = maximization(X, g)
+        if g is None or log is None or pi is None or m is None or S is None:
+            return None, None, None, None, None
+        if verbose and i % 10 == 0:
+            print(f"Log Likelihood after {i} iterations: {log.round(5)}")
+        if abs(log - l_prev) <= tol:
+            break
+
+    if verbose and not i % 10 == 0:
+        print(f"Log Likelihood after {i} iterations: {log.round(5)}")
+
+    return pi, m, S, g, log
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
+    """impo matplotlib.pyplot as plt
 
 
     np.random.seed(11)
     a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
     b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
     c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
-    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    d = np.random.multivariate_normal(
+        [20, 70], [[35, 10], [10, 35]], size=1000
+        )
     X = np.concatenate((a, b, c, d), axis=0)
     np.random.shuffle(X)
     k = 4
@@ -63,4 +86,4 @@ if __name__ == "__main__":
     print(X.shape[0] * pi)
     print(m)
     print(S)
-    print(l)
+    print(l)"""
