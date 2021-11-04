f35fd75cd00e3c1f44d16269f9d26d46b7c46e04
Ben Dosch <benjamindosch@gmail.com>
2021-11-03 19:32:26 -0500

    ready for checker

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -2,8 +2,8 @@
 """Module that contains the function gmm that calculates a GMM from a
 dataset."""
 
-import numpy as np
 import sklearn.mixture
+
 
 def gmm(X, k):
     """Function that calculates a GMM from a dataset.
@@ -11,7 +11,7 @@ def gmm(X, k):
     Args:
         X (numpy.ndarray): A tensor of shape (n, d) containing the dataset.
         k (int): The number of clusters.
-    
+
     Returns: pi, m, S, clss, bic
         pi (numpy.ndarray) A tensor of shape (k,) containing the cluster
             priors.
@@ -24,23 +24,26 @@ def gmm(X, k):
         bic (numpy.ndarray) A tensor of shape (kmax - kmin + 1) containing the
             BIC value for each cluster size tested.
     """
-    pi = []
-    m = []
-    S = []
-    clss = []
-    bic = []
+    gmm = sklearn.mixture.GaussianMixture(k).fit(X)
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
     return pi, m, S, clss, bic
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
+    """impo matplotlib.pyplot as plt
+    impo numpy as np
 
     np.random.seed(11)
     a = np.random.multivariate_normal([30, 40], [[75, 5], [5, 75]], size=10000)
     b = np.random.multivariate_normal([5, 25], [[16, 10], [10, 16]], size=750)
     c = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=750)
-    d = np.random.multivariate_normal([20, 70], [[35, 10], [10, 35]], size=1000)
+    d = np.random.multivariate_normal(
+        [20, 70], [[35, 10], [10, 35]], size=1000
+        )
     X = np.concatenate((a, b, c, d), axis=0)
     np.random.shuffle(X)
 
@@ -51,4 +54,4 @@ if __name__ == "__main__":
     print(bic)
     plt.scatter(X[:, 0], X[:, 1], s=10, c=clss)
     plt.scatter(m[:, 0], m[:, 1], s=50, marker='*', c=list(range(4)))
-    plt.show()
+    plt.show()"""
