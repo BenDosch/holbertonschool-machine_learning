6178ef662a95d3d024afe7019d92788fb65eaa40
Ben Dosch <benjamindosch@gmail.com>
2021-11-08 15:11:26 -0600

    ready for checker

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -11,25 +11,36 @@ def markov_chain(P, s, t=1):
     particular state after a specified number of iterations.
 
     Args:
-        P (numpy.ndarray): A square tensor of shape (n, n) representing the transition
-            matrix, where P[i, j] is the probability of transitioning from
-            state i to state j and n is the number of states in the markov
-            chain.
+        P (numpy.ndarray): A square tensor of shape (n, n) representing the
+            transition matrix, where P[i, j] is the probability of
+            transitioning from state i to state j and n is the number of states
+            in the markov chain.
         s (numpy.ndarray): A tensor of shape (1, n) representing the
             probability of starting in each state.
         t (int, optional): The number of iterations that the markov chain has
             been through. Defaults to 1.
-    
+
     Returns:
-        (numpy.ndarray): A tensor of shape (1, n) representing the probability
-            of being in a specific state after t iterations, or None on
-            failure.
+        s_k (numpy.ndarray): A tensor of shape (1, n) representing the
+            probability of being in a specific state after t iterations.
+        None on failure.
     """
-    return
+    if (not isinstance(P, np.ndarray) or P.ndim != 2 or
+            P.shape[0] != P.shape[1] or not isinstance(s, np.ndarray) or
+            s.ndim != 2 or s.shape[0] != 1 or s.shape[1] != P.shape[0] or
+            not isinstance(t, int) or t <= 0):
+        return None
+
+    s_k = s
+
+    for k in range(t):
+        s_k = s_k @ P
+
+    return s_k
 
 
 if __name__ == "__main__":
     P = np.array([[0.25, 0.2, 0.25, 0.3], [0.2, 0.3, 0.2, 0.3],
-        [0.25, 0.25, 0.4, 0.1], [0.3, 0.3, 0.1, 0.3]])
+                 [0.25, 0.25, 0.4, 0.1], [0.3, 0.3, 0.1, 0.3]])
     s = np.array([[1, 0, 0, 0]])
     print(markov_chain(P, s, 300))
