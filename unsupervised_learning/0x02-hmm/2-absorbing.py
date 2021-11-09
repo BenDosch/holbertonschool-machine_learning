aef0dc27ae0b2bb62b09e186f92ccc1e7fe8ba60
Ben Dosch <benjamindosch@gmail.com>
2021-11-09 12:59:23 -0600

    ready for checker

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -17,7 +17,45 @@ def absorbing(P):
     Returns:
         True if it is absorbing, or False on failure
     """
-    return
+    if (not isinstance(P, np.ndarray) or P.ndim != 2 or
+            P.shape[0] != P.shape[1]):
+        return False
+
+    n, _ = P.shape
+    diag = np.diag(P)
+
+    # Check if there is at least 1 absorbing state.
+    if not (diag == 1).any():
+        return False
+
+    # Check if all are absorbing state
+    if (diag == 1).all():
+        return True
+
+    # Put transition matrix in standard from. P is already in standard form.
+    # Get I, R, & Q from standard transition matrix P = [[I, O], [R, Q]]
+    I_size = np.where(diag != 1)[0][0]
+    I_from_P = np.eye(I_size)
+    R = P[I_size:, :I_size]
+    Q = P[I_size:, I_size:]
+
+    # Get F (fundimental matrix), F = Inverse(I - Q) where I.shape == Q.shape
+    I_for_Q = np.eye(Q.shape[0])
+    try:
+        F = np.linalg.inv(I_for_Q - Q)
+    except Exception:
+        return False
+
+    # Use F to find limiting matrix P_bar, P_bar = [[I, 0],[FR, 0]]
+    FR = F @ R
+    P_bar = np.zeros((n, n))
+    P_bar[:I_size, :I_size] = I_from_P
+    P_bar[I_size:, :I_size] = FR
+
+    if (FR == 0).all():
+        return False
+
+    return True
 
 
 if __name__ == "__main__":
