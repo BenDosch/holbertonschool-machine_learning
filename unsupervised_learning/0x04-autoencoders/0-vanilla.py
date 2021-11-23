121fe02ce3273c54bf3d3ce29095e77c2dd6ed96
Ben Dosch <benjamindosch@gmail.com>
2021-11-23 15:37:51 -0600

    style fixes and changes for checker

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -10,24 +10,57 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     Args:
         input_dims (int): An integer containing the dimensions of the model
             input.
-        hidden_layers (list[int]): A list containing the number of nodes for each
-            hidden layer in the encoder, respectively. The hidden layers are
-            reversed for the decoder.
-        latent_dims (int): An integer containing the dimensions of the latent space.
-    
+        hidden_layers (list[int]): A list containing the number of nodes for
+            each hidden layer in the encoder, respectively. The hidden layers
+            are reversed for the decoder.
+        latent_dims (int): An integer containing the dimensions of the latent
+            space.
+
     Returns:
         encoder(): The encoder model.
         decoder(): The decoder model.
         auto(): The full autoencoder model.
     """
-    model = keras.Sequential()
+    # Encoder
+    enco_in = keras.Input(shape=(input_dims,))
+    enco_hid = keras.layers.Dense(hidden_layers[0], activation='relu')(enco_in)
+    if len(hidden_layers) > 1:
+        for i in range(1, len(hidden_layers)):
+            enco_hid = keras.layers.Dense(
+                hidden_layers[i], activation='relu'
+            )(enco_hid)
+    enco_out = keras.layers.Dense(latent_dims, activation='relu')(enco_hid)
+
+    encoder = keras.models.Model(inputs=enco_in, outputs=enco_out)
+
+    # Decoder
+    deco_in = keras.Input(shape=(latent_dims,))
+    deco_hid = keras.layers.Dense(
+        hidden_layers[-1], activation='relu'
+    )(deco_in)
+    if len(hidden_layers) > 1:
+        for j in range(len(hidden_layers) - 2, -1, -1):
+            deco_hid = keras.layers.Dense(
+                hidden_layers[j], activation='relu'
+            )(deco_hid)
+    deco_out = keras.layers.Dense(input_dims, activation='sigmoid')(deco_hid)
+
+    decoder = keras.models.Model(deco_in, deco_out)
+
+    # Auto
+    auto_in = encoder(enco_in)
+    auto_out = decoder(auto_in)
+    auto = keras.models.Model(inputs=enco_in, outputs=auto_out)
+    auto.compile(loss='binary_crossentropy', optimizer='adam')
+
+    return encoder, decoder, auto
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import tensorflow as tf
-    from tensorflow.keras.datasets import mnist
+    """impo rt matplotlib.pyplot as plt
+    impo rt numpy as np
+    impo rt tensorflow as tf
+    fr om tensorflow.keras.datasets import mnist"""
 
     (x_train, _), (x_test, _) = mnist.load_data()
     x_train = x_train.astype('float32') / 255.
@@ -37,8 +70,8 @@ if __name__ == "__main__":
     np.random.seed(0)
     tf.set_random_seed(0)
     encoder, decoder, auto = autoencoder(784, [128, 64], 32)
-    auto.fit(x_train, x_train, epochs=50,batch_size=256, shuffle=True,
-                    validation_data=(x_test, x_test))
+    auto.fit(x_train, x_train, epochs=50, batch_size=256, shuffle=True,
+             validation_data=(x_test, x_test))
     encoded = encoder.predict(x_test[:10])
     print(np.mean(encoded))
     reconstructed = decoder.predict(encoded)
