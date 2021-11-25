7f4fae17f4da05b37597ca7788d296a6fb04f87f
Ben Dosch <benjamindosch@gmail.com>
2021-11-24 20:55:36 -0600

    seems to be working

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -1,22 +1,86 @@
 #!/usr/bin/env python3
-"""Module that contains """
+"""Module that contains a function that creates a variational autoencoder."""
 
 import tensorflow.keras as keras
+
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """Function that creates a variational autoencoder.
 
     Args:
-        input_dims (int): An integer containing the dimensions of the model input.
-        hidden_layers (list[int]): A list containing the number of nodes for each hidden layer in the encoder, respectively. The hidden layers should be reversed for the decoder.
-        latent_dims (int): An integer containing the dimensions of the latent space representation.
-    
+        input_dims (int): An integer containing the dimensions of the model
+            input.
+        hidden_layers (list[int]): A list containing the number of nodes for
+            each hidden layer in the encoder, respectively. The hidden layers
+            should be reversed for the decoder.
+        latent_dims (int): An integer containing the dimensions of the latent
+            space representation.
+
     Returns:
-        encoder(): The encoder model, which should output the latent representation, the mean, and the log variance, respectively.
-        decoder(): The decoder model.
-        auto(): The full autoencoder model.
+        encoder(keras.models.Model): The encoder model, which should output the
+            latent representation, the mean, and the log variance,
+            respectively.
+        decoder(keras.models.Model): The decoder model.
+        auto(keras.models.Model): The full autoencoder model.
     """
-    pass
+    # Encoder
+    enco_in = keras.Input(shape=(input_dims,))
+    enco_hid = keras.layers.Dense(hidden_layers[0], activation='relu')(enco_in)
+    if len(hidden_layers) > 1:
+        for i in range(1, len(hidden_layers)):
+            enco_hid = keras.layers.Dense(
+                hidden_layers[i], activation='relu'
+            )(enco_hid)
+
+    μ = keras.layers.Dense(latent_dims, activation=None)(enco_hid)
+    log_σ = keras.layers.Dense(latent_dims, activation=None)(enco_hid)
+
+    def sampler(args):
+        """sampler function for latent layer calculation"""
+        μ, log_σ = args
+        ε = keras.backend.random_normal(keras.backend.shape(μ))
+        return μ + keras.backend.exp(0.5 * log_σ) * ε  # latent_vector
+
+    z = keras.layers.Lambda(sampler)([μ, log_σ])
+
+    encoder = keras.models.Model(inputs=enco_in, outputs=(z, μ, log_σ))
+
+    # Decoder
+    deco_in = keras.Input(shape=(latent_dims,))
+    deco_hid = keras.layers.Dense(
+        hidden_layers[-1], activation='relu'
+    )(deco_in)
+    if len(hidden_layers) > 1:
+        for j in range(len(hidden_layers) - 2, -1, -1):
+            deco_hid = keras.layers.Dense(
+                hidden_layers[j], activation='relu'
+            )(deco_hid)
+    deco_out = keras.layers.Dense(input_dims, activation='sigmoid')(deco_hid)
+
+    decoder = keras.models.Model(deco_in, deco_out)
+
+    # Auto
+    auto_in = encoder(enco_in)
+    auto_out = decoder(auto_in)
+    auto = keras.models.Model(inputs=enco_in, outputs=auto_out)
+
+    def KL_loss(inputs, outputs):
+        """cost function"""
+        loss = keras.backend.binary_crossentropy(inputs, outputs)
+        loss = keras.backend.sum(loss, axis=1)
+        KL_divergence = (
+            -0.5 * keras.backend.sum(
+                1 + log_σ - keras.backend.square(μ) - keras.backend.exp(log_σ),
+                axis=-1
+            )
+        )
+        return loss + KL_divergence
+
+    auto.compile(
+        loss=KL_loss, optimizer='adam'
+    )
+
+    return encoder, decoder, auto
 
 
 if __name__ == "__main__":
@@ -34,7 +98,7 @@ if __name__ == "__main__":
     tf.set_random_seed(0)
     encoder, decoder, auto = autoencoder(784, [512], 2)
     auto.fit(x_train, x_train, epochs=50, batch_size=256, shuffle=True,
-                    validation_data=(x_test, x_test))
+             validation_data=(x_test, x_test))
     encoded, mu, log_sig = encoder.predict(x_test[:10])
     print(mu)
     print(np.exp(log_sig / 2))
@@ -49,7 +113,6 @@ if __name__ == "__main__":
         ax.axis('off')
         plt.imshow(reconstructed[i])
     plt.show()
-
 
     l1 = np.linspace(-3, 3, 25)
     l2 = np.linspace(-3, 3, 25)
