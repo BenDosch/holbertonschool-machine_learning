c0bbfdccf0d5bf476ccd4479a350c0254e284871
Ben Dosch <benjamindosch@gmail.com>
2022-02-15 17:47:56 -0600

    Ready for checker

--- reinforcement_learning/0x02-temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/0x02-temporal_difference/0-monte_carlo.py
@@ -8,11 +8,11 @@ import numpy as np
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
                 gamma=0.99):
-    """Function that preforms the Monte Carlo algorithm.
+    """Function that preforms the Online First-Visit Monte Carlo algorithm.
 
     Args:
         env (_type_): The openAI environment instance.
-        V (numpy.ndarray): A tensor of shape (s,) containing the value
+        V (numpy.ndarray): A tensor of shape (state,) containing the value
             estimate.
         policy (function): A function that takes in a state and returns the
             next.
@@ -25,10 +25,35 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
         gamma (float, optional): The discount rate. Defaults to 0.99.
 
     Returns:
-        V (numpy.ndarray): A tensor of shape (s,) containing the updated
+        V (numpy.ndarray): A tensor of shape (state,) containing the updated
             value estimate.
     """
-    # Code goes here
+    # Sample episodes
+    for episode in range(episodes):
+        state = env.reset()
+        results = []
+        for step in range(max_steps):
+            action = policy(state)
+            next_state, reward, done, _ = env.step(action)
+            results.append([state, reward])
+            if done:
+                break
+            state = next_state
+
+        results = np.array(results, dtype=int)
+
+        # Update Value estimate
+        # V(St) = V(St) + alpha * (Gt - V(St))
+        Gt = 0
+        for step, result in enumerate(results[::-1]):
+            state, reward = result
+            Gt = (gamma * Gt) + reward
+
+            # If
+            if state not in results[:episode, 0]:
+                V[state] = V[state] + (alpha * (Gt - V[state]))
+
+    return V
 
 
 if __name__ == "__main__":
@@ -59,18 +84,18 @@ if __name__ == "__main__":
                 return UP
 
     V = np.where(env.desc == b'H', -1, 1).reshape(64).astype('float64')
-    np.set_printoptions(precision=2)
+    np.set_printoptions(precision=4)
     env.seed(0)
     test = monte_carlo(env, V, policy).reshape((8, 8))
     print(test)
 
     Exp = """[[ 0.81    0.9     0.4783  0.4305  0.3874  0.4305  0.6561  0.9   ]
-     [ 0.9     0.729   0.5905  0.4783  0.5905  0.2824  0.2824  0.3874]
-     [ 1.      0.5314  0.729  -1.      1.      0.3874  0.2824  0.4305]
-     [ 1.      0.5905  0.81    0.9     1.     -1.      0.3874  0.6561]
-     [ 1.      0.6561  0.81   -1.      1.      1.      0.729   0.5314]
-     [ 1.     -1.     -1.      1.      1.      1.     -1.      0.9   ]
-     [ 1.     -1.      1.      1.     -1.      1.     -1.      1.    ]
-     [ 1.      1.      1.     -1.      1.      1.      1.      1.    ]]"""
+ [ 0.9     0.729   0.5905  0.4783  0.5905  0.2824  0.2824  0.3874]
+ [ 1.      0.5314  0.729  -1.      1.      0.3874  0.2824  0.4305]
+ [ 1.      0.5905  0.81    0.9     1.     -1.      0.3874  0.6561]
+ [ 1.      0.6561  0.81   -1.      1.      1.      0.729   0.5314]
+ [ 1.     -1.     -1.      1.      1.      1.     -1.      0.9   ]
+ [ 1.     -1.      1.      1.     -1.      1.     -1.      1.    ]
+ [ 1.      1.      1.     -1.      1.      1.      1.      1.    ]]"""
 
-    print("Passed") if test == Exp else print("Failed")
+    print("Passed") if str(test) == Exp else print("Failed")
